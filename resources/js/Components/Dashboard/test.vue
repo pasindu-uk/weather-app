01a8f724e1dc65404063699aade1186cbe830fc7
Pasindu Premaratne <pasindu.uk@gmail.com>
2022-04-02 21:53:17 +0530

    Initial Setup

---
+++ resources/js/Components/Dashboard/test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Test</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    mounted() {
+        console.log('Property Selector Mounted.');
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    created() {
+    },
+
+    methods: {
+    }
+}
+</script>
