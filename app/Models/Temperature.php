7bb4f2b032ae59a8b459a00af751dfba58ca7c93
Pasindu Premaratne <pasindu.uk@gmail.com>
2022-04-03 09:07:40 +0530

    Implementation of model and migration to store temperetures

---
+++ app/Models/Temperature.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Temperature extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'timestamp',
+        'celsius',
+        'fahrenheit',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'timestamp' => 'datetime',
+    ];
+}
---
+++ database/migrations/2022_04_03_031333_create_temperatures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemperaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temperatures', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamp('timestamp');
+            $table->float('celsius');
+            $table->float('fahrenheit');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temperatures');
+    }
+}
